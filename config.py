f414cca5e02fe3f932bebed4e2dd455bd901c4a5
Apnimaa <patelkinjal1212@gmail.com>
2025-12-27 22:06:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7690060051:AAHJOn2nJkmDBXqQCLLQLuJ1Y8ZkkOaiF1Y")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", "26468828"))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")  # Added key name for consistency
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8258136393:AAHDc3vCtgT_MVS4flPCzOw-IxBNvfXXoU8")  # Ensure correct key name
+    API_ID = int(os.environ.get("API_ID", "27433400"))  # Added key name and default value
+    API_HASH = os.environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "7517045929").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "6201066540").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "http://master-api-v3.vercel.app")  # Keeping HOST configurable
     CREDIT = os.environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")  # Making CREDIT an environment variable for flexibility
+
